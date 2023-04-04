fa2c972c8c835d9347c9bbfc3280da9a672a30a0
Vanshika-02 <vanshusrivastava2002@gmail.com>
2023-04-04 16:01:25 +0530

    login route changed

--- routes/login.js
+++ routes/login.js
@@ -11,7 +11,7 @@ const tokenUtils = require('../utils/sessionTokenUtil');    // to manage tokens
 
 router.get('/', (req, res) => {
   // res.sendFile('./../public/loginPage.html');
-  res.sendFile(path.join(__dirname, '/../public/loginPage.html'));
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
 router.post('/', (req, res) => {
