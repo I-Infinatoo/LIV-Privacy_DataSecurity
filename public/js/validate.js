c3894c217dc846b7baf72dfb9b46451dcc048724
Infinatoo <ishurajan234@gmail.com>
2023-04-12 23:31:59 +0530

    added new background added check for the confirm password

--- public/js/validate.js
+++ public/js/validate.js
@@ -2,6 +2,7 @@ function validateForm() {
 
     const email = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
+    const confirmPassword = document.querySelector("#cpassword").value;
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
@@ -12,6 +13,12 @@ function validateForm() {
     }
     if(!passwordRegex.test(password)) {
         alert("Invalid password format");
+        return false;
+    }
+
+    // during signup page, reconfirm the password
+    if(confirmPassword && confirmPassword !== password) {
+        alert("Password and confirm Password is not same.");
         return false;
     }
 }
