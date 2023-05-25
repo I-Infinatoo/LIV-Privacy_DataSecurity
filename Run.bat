2750e063b0bc334078d8f3f0d599088d0e66b3d1
Infinatoo <ishurajan234@gmail.com>
2023-05-26 00:00:46 +0530

    minor change in run.bat

--- Run.bat
+++ Run.bat
@@ -1,21 +1,21 @@
 @echo off
 
-REM Check for Java
+@REM REM Check for Java
 java -version 2>nul
 if %errorlevel% neq 0 (
     echo Java is not available
     exit /b
 ) else (
-    echo Java is available
+    @REM echo Java is available
 )
 
-REM Check for Node.js
+@REM REM Check for Node.js
 node -v >nul 2>&1
 if %errorlevel% neq 0 (
     echo Node.js is not available
     exit /b
 ) else (
-    echo Node.js is available
+    @REM echo Node.js is available
     npm i
-    npm run dev
+    npm run start
 )
